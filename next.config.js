8eb87d682e534f241447f0a87d73876c0c0a2bbc
Yi Zhang <526679173@qq.com>
2023-01-27 13:53:20 +1000

    allow access

--- next.config.js
+++ next.config.js
@@ -5,7 +5,12 @@ const nextConfig = {
   },
   reactStrictMode: true,
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 }
 
